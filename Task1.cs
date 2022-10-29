1317cc11fda66b82a6591fcdd15a5d539816f663
Матвієнко Артем <artemm3notegp@gmail.com>
2022-10-29 22:31:46 +0300

    Прототип програми на рівні заглушок

---
+++ Task1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+class Card
+{
+    static void Main(string[] args)
+    {
+        int q = 1;
+        double d = 3.2;
+        Console.WriteLine(q + d) ;
+        double x = 0;
+        double y = 0;
+        double z = 0;
+
+        double a = 0;
+        double b = 0;
+    }
+
+}
