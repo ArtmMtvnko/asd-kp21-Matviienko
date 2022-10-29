a40d270ab82b3c2abb2d31fc952c5bac7a09c11f
Матвієнко Артем <artemm3notegp@gmail.com>
2022-10-30 00:28:39 +0300

    виправлення помилок, друга задача має обмеження на від'ємні числа, додані коментарі до обох задач, додан гітігнор

--- Task1.cs
+++ Task1.cs
@@ -5,8 +5,10 @@ class Card
 {
     static void Main(string[] args)
     {
+        // константа для роботі з тригонометричними функціями
         const int pi = 180;
 
+        // Введення данних
         Console.WriteLine("Enter x: ");
         double x = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("Enter y: ");
@@ -17,45 +19,41 @@ class Card
         double a = 0;
         double b = 0;
 
+        // перевірка на ОДЗ
         if (x * z == -2)
         {
             Console.WriteLine("Error");
         }
         else
         {
+            // обчисленя значення кута
             double angleTan = (Math.Pow(x, (-y)) + (z / (y * y + 1)) + Math.Round(Math.Pow(y / (x * z + 2), (0.333)), 1));
+            // переведення кута у радіани
             double radianTan = angleTan * (Math.PI / 180);
+            // знаходимо значення "а"
             a = Math.Tan(radianTan);
         }
 
+        // обчисленя значення кута та радіану
         double angleSin = (x + pi * y) / z;
         double radianSin = angleSin * (Math.PI / 180);
 
+        // перевірка на ОДЗ
         if (Math.Sin(radianSin) == 0)
         {
             Console.WriteLine("Error");
         }
         else
         {
-
+            // обчислюємо значення "b"
             b = a / Math.Sin(radianSin);
         }
 
+        // Округлення та виведення значень 
         a = Math.Round(a, 4);
         b = Math.Round(b, 4);
         Console.WriteLine("a = " + a);
         Console.WriteLine("b = " + b);
-
-
-
-        /* x = 2, y = 1, z = 3 
-         * 
-         * ===operations start====
-         * a = tan(x^-y + z/(x^2 + 1) + (y/(xz + 2))^1/3)
-         * b = a/(sin((x+pi*y)/z)
-         * ===perations end====
-         * 
-         * output a = 0.04366, b = 0.05*/
     }
 
 }
--- Task2.cs
+++ Task2.cs
@@ -10,26 +10,34 @@ class Card
         Console.WriteLine("Enter the number: ");
         n = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Palindromes are : ");
-        // Цикл який пробігається по натуральним числам до n
-        for (int i = 1; i <= n; i++)
+        if (n < 0)
         {
-            // возводиму у квадрат та конвертуємо число у строку для зручної перевідки на паліндром
-            string iModivite = (i * i).ToString();
-            int L = iModivite.Length;
-            string container = "";
+            Console.WriteLine("Error of input:/");
+        }
+        else
+        {
+            Console.WriteLine("Palindromes are : ");
+            // Цикл який пробігається по натуральним числам до n
+            for (int i = 1; i <= n; i++)
+            {
+                // возводиму у квадрат та конвертуємо число у строку для зручної перевідки на паліндром
+                string iModivite = (i * i).ToString();
+                int L = iModivite.Length;
+                string container = "";
 
-            // Цикл який відзеркалює число
-            for (int j = L - 1; j >= 0; j--)
-            {
-                container = container + iModivite[j];
-            }
-            // якщо відзеркалене число = початковому, число є паліндромом, виводимо який це номер
-            if (iModivite == container)
-            {
-                Console.WriteLine(i);
+                // Цикл який відзеркалює число
+                for (int j = L - 1; j >= 0; j--)
+                {
+                    container = container + iModivite[j];
+                }
+                // якщо відзеркалене число = початковому, число є паліндромом, виводимо який це номер
+                if (iModivite == container)
+                {
+                    Console.WriteLine(i);
+                }
             }
         }
+
     }
 
 }
