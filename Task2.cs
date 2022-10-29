32f3c4a397c0d5a9f6bd75ab706dd8a61596bf97
Матвієнко Артем <artemm3notegp@gmail.com>
2022-10-30 00:22:40 +0300

    Готове рішення для другої задачі

--- Task2.cs
+++ Task2.cs
@@ -5,28 +5,31 @@ class Card
 {
     static void Main(string[] args)
     {
-        // input
-        int n = 100;
+        // Вводимо змінну
+        int n = 0;
+        Console.WriteLine("Enter the number: ");
+        n = Convert.ToInt32(Console.ReadLine());
 
-        /*
-         * 1^2 = 1 - it's palindrome
-         * 2^2 = 4 - it's palindrome
-         * 3^2 = 9 - it's palindrome
-         * 4^2 = 16 - it is not palindrome
-         * .....
-         * 11^2 = 121 - it's palindrome
-         * 12^2 = 144 - it is not palindrome
-         * .....
-         * 
-         * ===operation===
-         * for (int i = 1; i <= n; i++) 
-         * {
-         *      ........
-         * }
-         * ============
-         * 
-         * output: 1, 2, 3, 11
-         */
+        Console.WriteLine("Palindromes are : ");
+        // Цикл який пробігається по натуральним числам до n
+        for (int i = 1; i <= n; i++)
+        {
+            // возводиму у квадрат та конвертуємо число у строку для зручної перевідки на паліндром
+            string iModivite = (i * i).ToString();
+            int L = iModivite.Length;
+            string container = "";
+
+            // Цикл який відзеркалює число
+            for (int j = L - 1; j >= 0; j--)
+            {
+                container = container + iModivite[j];
+            }
+            // якщо відзеркалене число = початковому, число є паліндромом, виводимо який це номер
+            if (iModivite == container)
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 
 }
