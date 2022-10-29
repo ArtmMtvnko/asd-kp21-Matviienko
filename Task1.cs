d33b088cb559f898c8a04476f1b16b3e0b2f1bb7
Матвієнко Артем <artemm3notegp@gmail.com>
2022-10-29 22:34:21 +0300

    прототип програми на рівні заглушок

--- Task1.cs
+++ Task1.cs
@@ -5,15 +5,23 @@ class Card
 {
     static void Main(string[] args)
     {
-        int q = 1;
-        double d = 3.2;
-        Console.WriteLine(q + d) ;
+        const int pi = 180;
+
         double x = 0;
         double y = 0;
         double z = 0;
 
         double a = 0;
         double b = 0;
+        /* x = 2, y = 1, z = 3 
+         * 
+         * ===operations start====
+         * a = tan(x^-y + z/(x^2 + 1) + (y/(xz + 2))^1/3)
+         * b = a/(sin((x+pi*y)/z)
+         * ===perations end====
+         * 
+         * output a = 0.04366, b = */
+
     }
 
 }
