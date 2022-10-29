5c4840a10faa98d5c021a7c42ca424810a272530
Матвієнко Артем <artemm3notegp@gmail.com>
2022-10-29 23:16:53 +0300

    тест першої задачі, результат відповідає очікуванням

--- Task1.cs
+++ Task1.cs
@@ -7,12 +7,28 @@ class Card
     {
         const int pi = 180;
 
-        double x = 0;
-        double y = 0;
-        double z = 0;
+        double x = 2;
+        double y = 1;
+        double z = 3;
 
         double a = 0;
         double b = 0;
+
+        double angleTan = (Math.Pow(x, (-y)) + (z / (y * y + 1)) + Math.Round(Math.Pow(y / (x * z + 2), (0.333)), 1));
+        double radianTan = angleTan * (Math.PI / 180);
+        a = Math.Tan(radianTan);
+
+        double angleSin = (x + pi * y) / z;
+        double radianSin = angleSin * (Math.PI / 180);
+        b = Math.Sin(radianSin);
+
+        a = Math.Round(a, 4);
+        b = Math.Round(b, 4);
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+
+
+
         /* x = 2, y = 1, z = 3 
          * 
          * ===operations start====
@@ -20,7 +36,7 @@ class Card
          * b = a/(sin((x+pi*y)/z)
          * ===perations end====
          * 
-         * output a = 0.04366, b = */
+         * output a = 0.04366, b = 0.871*/
 
     }
 
