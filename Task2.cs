35cc4cc8224a2156df7f430a62ab4de17dad2eba
Матвієнко Артем <artemm3notegp@gmail.com>
2022-10-30 00:11:00 +0300

    прототип ДРУГОЇ задачі на рівні заглушок

---
+++ Task2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+class Card
+{
+    static void Main(string[] args)
+    {
+        // input
+        int n = 100;
+
+        /*
+         * 1^2 = 1 - it's palindrome
+         * 2^2 = 4 - it's palindrome
+         * 3^2 = 9 - it's palindrome
+         * 4^2 = 16 - it is not palindrome
+         * .....
+         * 11^2 = 121 - it's palindrome
+         * 12^2 = 144 - it is not palindrome
+         * .....
+         * 
+         * ===operation===
+         * for (int i = 1; i <= n; i++) 
+         * {
+         *      ........
+         * }
+         * ============
+         * 
+         * output: 1, 2, 3, 11
+         */
+    }
+
+}
